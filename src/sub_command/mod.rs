e3fb5b225cf9306466d78c3e251428348ad9bfcf
Kyohei Hamaguchi (@tnzk) <tnzk@users.noreply.github.com>
2022-06-15 00:39:40 +0900

    Add -v, -h and mockup of new subcommand

---
+++ src/sub_command/mod.rs
@@ -0,0 +1,12 @@
+pub fn create_new_project(project_name: &String) -> () {
+  println!("You're gonna get: project {}", project_name)
+  /*
+  Do equivalent of:
+  npm init svelte $project_name
+  pnpm install
+  pnpm i @svelteuidev/core @svelteuidev/composables
+  pnpm dev
+  updating __layout
+  ... or maybe just fetch a prepared template
+   */
+}
